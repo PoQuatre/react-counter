6c23801021e3c59ff7a3ba4179ddddf4a3c6407c
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-23 15:08:47 +0100

    Add a 2nd Counter and link it to the 1st

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,39 @@ import { useState } from "react";
 import Counter from "./components/Counter";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [firstCount, setFirstCount] = useState(0);
+  const [secondCount, setSecondCount] = useState(0);
 
-  const decrementCount = () => setCount(Math.max(0, count - 1));
-  const incrementCount = () => setCount(count + 1);
+  const decrementFirstCount = () => {
+    setFirstCount(Math.max(0, firstCount - 1));
+  };
+  const incrementFirstCount = () => {
+    const value = Math.min(100, firstCount + 1);
+    setFirstCount(value);
+    value > secondCount && setSecondCount(value);
+  };
+
+  const decrementSecondCount = () => {
+    const value = Math.max(0, secondCount - 1);
+    setSecondCount(value);
+    value < firstCount && setFirstCount(value);
+  };
+  const incrementSecondCount = () => {
+    setSecondCount(Math.min(100, secondCount + 1));
+  };
 
   return (
     <>
       <h1>Counter</h1>
       <Counter
-        count={count}
-        onAdd={incrementCount}
-        onSubtract={decrementCount}
+        count={firstCount}
+        onAdd={incrementFirstCount}
+        onSubtract={decrementFirstCount}
+      />
+      <Counter
+        count={secondCount}
+        onAdd={incrementSecondCount}
+        onSubtract={decrementSecondCount}
       />
     </>
   );

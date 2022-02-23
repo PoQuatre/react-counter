e495728b2baa83358920359246e42d51fcaeb31d
PoQuatre <leflem.mateo2002@gmail.com>
2022-02-23 14:50:06 +0100

    Add implementations for incrementCount & decrementCount

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { useState } from "react";
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const decrementCount = () => {};
-  const incrementCount = () => {};
+  const decrementCount = () => setCount(count - 1);
+  const incrementCount = () => setCount(count + 1);
 
   return (
     <>
       <h1>Counter</h1>
       <h2>{count}</h2>
       <div className="button-group">
-        <button className="decrement">-</button>
-        <button className="increment">+</button>
+        <button className="decrement" onClick={decrementCount}>
+          -
+        </button>
+        <button className="increment" onClick={incrementCount}>
+          +
+        </button>
       </div>
     </>
   );
